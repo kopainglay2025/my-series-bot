3faa35681425de21d8474e6ae9e86f62a86ea92d
kopainglay2025 <100895177+kopainglay2025@users.noreply.github.com>
2025-12-13 00:36:23 +0630

    Update files.py

--- src/database/utils/files.py
+++ src/database/utils/files.py
@@ -8,21 +8,20 @@ from pyrogram.file_id import FileId
 from src.database import db, instance
 from config import COLLECTION_NAME
 
-
-
-
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
+    file_id = fields.StringField(attribute='_id', required=True)
+    file_ref = fields.StringField(allow_none=True)
+    file_name = fields.StringField(required=True)
+    file_size = fields.IntegerField(required=True)
+    file_type = fields.StringField(allow_none=True)
+    mime_type = fields.StringField(allow_none=True)
+    caption = fields.StringField(allow_none=True)
+
     class Meta:
         indexes = ('$file_name', )
         collection_name = COLLECTION_NAME
+
 
 # -------------------------------
 # Database Utilities
